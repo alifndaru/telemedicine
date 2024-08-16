d973fb46d1e65cd23489868c90773a57eca20939
alifndaru <alifndaru8@gmail.com>
2024-08-16 19:06:42 +0700

    refactor: Update psikolog_list.php to display doctor's specialization and improve code readability

--- application/views/phpmu-ekonseling/psikolog_list.php
+++ application/views/phpmu-ekonseling/psikolog_list.php
@@ -2,36 +2,37 @@
 	<div class="provider-sidebar">
 		<h3>DATA PROVIDER KAMI</h3>
 		<ul>
-			<!-- <li><a href="#">Dokter Spesialis</a></li> -->
 			<?php foreach ($jabatan as $data): ?>
 				<li>
-					<a href="#">
-						<?= htmlspecialchars($data['jabatan'], ENT_QUOTES, 'UTF-8'); ?>
+					<a href="<?= base_url('psikolog/lists?spesialis=' . urlencode($data['spesialis'])); ?>">
+						<?= htmlspecialchars($data['spesialis'], ENT_QUOTES, 'UTF-8'); ?>
 					</a>
 				</li>
 			<?php endforeach; ?>
+			<!-- Tambahkan item untuk 'Semua Spesialis' -->
+			<li>
+				<a href="<?= base_url('psikolog/lists'); ?>">Semua Spesialis</a>
+			</li>
 		</ul>
 	</div>
 	<div class="provider-content">
-		<?php foreach ($psikolog->result_array() as $row) {
-		?>
+		<?php foreach ($doctors as $row): ?>
 			<div class="provider-item">
-				<img src="<?php echo base_url('asset/foto_user/' . trim($row['foto_dokter'])); ?>" alt="">
+				<img src="<?= base_url('asset/foto_user/' . trim($row['foto'])); ?>" alt="">
 				<div class="provider-info">
 					<h4><?= $row['nama_lengkap'] ?></h4>
-					<p class="specialization"><?= $row['jabatan'] ?></p>
-					<p class="location"><?= $row['klinik'] ?></p>
-					<p class="address"><?= $row['alamat_klinik'] ?>, <?= $row['provinsi'] ?></p>
+					<p class="specialization">Jabatan: <?= $row['perangkat_daerah'] ?></p>
+					<p class="location">Spesialis: <?= $row['spesialis'] ?></p>
+					<p class="address">Klinik: <?= $row['klinik'] ?></p>
 				</div>
 				<div class="provider-buttons">
 					<button class="profile-btn">PROFIL</button>
 					<button class="consult-btn">KONSULTASI</button>
 				</div>
 			</div>
-		<?php } ?>
-		<!-- Tambahkan item provider lainnya sesuai kebutuhan -->
+		<?php endforeach; ?>
 		<div class="pagination">
-			<?php echo $this->pagination->create_links(); ?>
+			<?= $this->pagination->create_links(); ?>
 		</div>
 	</div>
 </div>
